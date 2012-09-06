1cb263d589d75ab6b3bc0a1182cccba73c6f313a
Guillermo Álvarez <guillermo@cientifico.net>
2012-09-06 16:38:21 +0200

    Added support for multiple commands ala git
    To define new command just inherit from SS2JSON::Command
    
    This is an example
    
        require 'ss2json/command'
    
        class ToHash < SS2JSON::Command
    
          def self.name
            "to_hash"
          end
    
          def self.description
            "Convert a spreadsheet to a json hash"
          end
    
          def initialize(options)
            puts options.inspect
          end
    
        end
    
    From the command line point of view, it acts as git.
    All the commands should be stored in 'ss2json-commands/command_name.rb'
    and the name should be equal to the file name. The commands could be in
    different rubygems.

---
+++ lib/ss2json/command.rb
@@ -0,0 +1,23 @@
+
+
+module SS2JSON
+  class Command
+
+    @commands = []
+    def self.load_all
+      require 'rubygems' unless defined?(Gem)
+      Gem.find_files('ss2json-commands/*.rb').each do |file|
+        require(file)
+      end
+      @commands
+    end
+
+    def self.commands
+      @commands
+    end
+
+    def self.inherited(kclass)
+      @commands << kclass
+    end
+  end
+end
